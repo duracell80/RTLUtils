13804ada99507fbef550f7907c149782a43019bf
Lee Jordan <duracell80@users.noreply.github.com>
2020-06-02 15:12:23 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt-get install -y  sox git cmake pkg-config libusb-1.0-0-dev
+
+git clone git://git.osmocom.org/rtl-sdr.git
+cd ./rtl-sdr
+mkdir build
+cd ./build
+cmake ../ -DINSTALL_UDEV_RULES=ON
+make
+sudo make install
+sudo cp ../rtl-sdr.rules /etc/udev/rules.d/
+sudo ldconfig
+
+sudo cp ../blacklist.rtl.conf /etc/modprobe.d/
+sudo reboot
