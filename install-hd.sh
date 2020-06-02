fc153aabb4bbefa271ba29ba4dabf93a9cf8348a
Lee Jordan <duracell80@users.noreply.github.com>
2020-06-02 16:00:26 -0500

    Create install-hd.sh

---
+++ install-hd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt install git build-essential cmake autoconf libtool libao-dev libfftw3-dev librtlsdr-dev
+
+git clone https://github.com/theori-io/nrsc5.git
+cd nrsc5
+mkdir build
+cd build
+cmake -DUSE_COLOR=ON
+make
+sudo make install
+sudo ldconfig
