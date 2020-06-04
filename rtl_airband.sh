928428b57f7299daaba7dfed28b0c6a6106d46e2
Lee Jordan <duracell80@users.noreply.github.com>
2020-06-03 23:50:42 -0500

    Update rtl_airband.sh

--- rtl_airband.sh
+++ rtl_airband.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rtl_fm -g 50 -f 118M:137M:32k -M am -s 32k -l 70 |  play -r 32k -t raw -e s -b 16 -c 1 -V1 - lowpass 16k
+rtl_fm -g 50 -f 118M:137M:32k -M am -s 32k -l 70 -t 1 |  play -r 32k -t raw -e s -b 16 -c 1 -V1 - lowpass 16k
