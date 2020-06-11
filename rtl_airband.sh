70d593fe024fc61637a23b4ce924823815cddd20
Lee Jordan <duracell80@users.noreply.github.com>
2020-06-10 21:20:05 -0500

    Update rtl_airband.sh

--- rtl_airband.sh
+++ rtl_airband.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo systemctl stop rtl_fm.service
-rtl_fm -g 50 -f 118M:137M:32k -M am -s 32k -l 70 -t 1 |  play -r 32k -t raw -e s -b 16 -c 1 -V1 - lowpass 16k
+sudo mpc stop
+rtl_fm -g 50 -f 118M:122M:32k -M am -s 32k -l 70 -t 1 |  play -r 32k -t raw -e s -b 16 -c 1 -V1 - lowpass 16k
