1f7e159a56671d187d468d63fb912ae0a4545f1b
Lee Jordan <duracell80@users.noreply.github.com>
2020-07-06 23:41:16 -0500

    Update install-hd.sh

--- install-hd.sh
+++ install-hd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install -y git build-essential cmake autoconf libtool libao-dev libfftw3-dev librtlsdr-dev
+sudo apt install -y git build-essential cmake autoconf libtool libao-dev libfft>
 
 git clone https://github.com/theori-io/nrsc5.git
 cd nrsc5
@@ -9,3 +9,10 @@ cmake ../ -DUSE_NEON=ON -DINSTALL_UDEV_RULES=ON
 make
 sudo make install
 sudo ldconfig
+
+sudo apt-get install python3-pyaudio
+sudo apt install python3-gi python3-gi-cairo gir1.2-gtk-3.0
+git clone https://github.com/cmnybo/nrsc5-gui.git
+cd nrsc5-gui
+python3 nrsc5_gui.py
+
